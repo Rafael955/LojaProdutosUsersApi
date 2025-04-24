957827c49687f82e69f36e6d41d1f9aa1daf0059
Rafael955 <rafaelcaffonso@gmail.com>
2025-04-24 20:50:37 -0300

    Correção da Senha colocando-a criptografada para SHA256

--- UsersApi.Infra.Data/Mappings/UsuarioMap.cs
+++ UsersApi.Infra.Data/Mappings/UsuarioMap.cs
@@ -62,7 +62,7 @@ namespace UsersApi.Infra.Data.Mappings
                     Id = Guid.Parse("C3610A1A-1FB1-44A6-A7A4-311D05C3C61D"), 
                     Nome = "Admin",
                     Email = "admin@lojaprodutosapp.com",
-                    Senha = "Admin@123",
+                    Senha = "e86f78a8a3caf0b60d8e74e5942aa6d86dc150cd3c03338aef25b7d2d7e3acc7", //Admin@123
                     Status = Status.Ativo,
                     PerfilId = Guid.Parse("7108F98B-F360-40FC-8A87-4C4D2BF578F5")
                 }
